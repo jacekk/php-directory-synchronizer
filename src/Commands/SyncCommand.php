4d97bb9b4d8be4e69e68d9c1741213d5bc5e0738
JacekK <info@jacek-k.net>
2014-09-30 01:53:47 +0200

    added logic for file listing, comparing, removing and copying

--- src/Commands/SyncCommand.php
+++ src/Commands/SyncCommand.php
@@ -8,13 +8,20 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
 
 class SyncCommand extends Command
 {
+    const CHECKSUM_OFFSET = 34;
+
     private $src;
+    private $srcFiles;
+
     private $dest;
-    private $fs;
+    private $destFiles;
+
+    private $fs; // Filesystem
     private $output;
 
     protected function configure()
@@ -36,15 +43,15 @@ class SyncCommand extends Command
         $this->processSync();
     }
 
-    protected function prepareProperties(InputInterface $input, OutputInterface $output)
+    private function prepareProperties(InputInterface $input, OutputInterface $output)
     {
         $this->src = $input->getArgument('<src>');
         $this->dest = $input->getArgument('<dest>');
-        $this->fs = new Filesystem();
         $this->output = $output;
+        $this->fs = new Filesystem();
     }
 
-    protected function pathsAreValid()
+    private function pathsAreValid()
     {
         if (! $this->fs->exists($this->src)) {
             $this->output->writeln('<error><src> is not a directory</error>');
@@ -60,8 +67,72 @@ class SyncCommand extends Command
         return true;
     }
 
-    protected function processSync()
+    private function processSync()
     {
-        $this->output->writeln('@todo implement processSync');
+        $this->srcFiles = $this->listFiles($this->src);
+        $this->destFiles = $this->listFiles($this->dest);
+
+        $filesToRemove = array_diff($this->destFiles, $this->srcFiles);
+        $filesToCopy = array_diff($this->srcFiles, $this->destFiles);
+
+        $this->removeOldFiles($filesToRemove);
+        $this->copyNewFiles($filesToCopy);
+        // $this->removeEmptyDirectories();
+    }
+
+    private function listFiles($directory)
+    {
+        $out = array();
+
+        $finder = new Finder();
+        $iterator = $finder
+            ->files()
+            ->in($directory);
+
+        foreach ($iterator as $file) {
+            $fullPath = $file->getRealpath();
+            $fullPath = str_replace('\\', '/', $fullPath);
+            $checksum = md5_file($fullPath);
+
+            $relPath = $file->getRelativePathname();
+            $relPath = str_replace('\\', '/', $relPath);
+
+            $out[] = sprintf('%s__%s', $checksum, $relPath);
+        }
+        return $out;
+    }
+
+    private function removeOldFiles($files)
+    {
+        $list = array();
+        foreach ($files as $file) {
+            $fileName = substr($file, static::CHECKSUM_OFFSET);
+            $path = $this->dest . $fileName;
+            $list[] = $path;
+        }
+        try {
+            $this->fs->remove($list);
+            $counter = count($list);
+            $this->output->writeln("removed old files: {$counter}");
+        } catch (Exception $ex) {
+            $this->output->writeln(sprintf('<error>%s</error>', $ex->getMessage()));
+        }
+    }
+
+    private function copyNewFiles($files)
+    {
+        $counter = 0;
+        foreach ($files as $file) {
+            $fileName = substr($file, static::CHECKSUM_OFFSET);
+            $srcPath = $this->src . $fileName;
+            $destPath = $this->dest . $fileName;
+            try {
+                $this->fs->copy($srcPath, $destPath, true);
+                $counter++;
+            } catch (Exception $ex) {
+                $this->output->writeln(sprintf('<error>Could not copy: %s</error>', $fileName));
+            }
+        }
+        $this->output->writeln("copied new files: {$counter}");
     }
 }
